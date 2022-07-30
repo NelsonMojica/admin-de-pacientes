6632059458bc497fafd496b2fbb88ff9c53000ad
NelsonMojica <tecmec777@gmail.com>
2022-07-29 19:10:40 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState, } from 'react'
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
+//citas en local storage
+let citasIniciales = JSON.parse(localStorage.getItem('citas'))
+if (!citasIniciales){
+  citasIniciales= []
+}
+  //aqui voy a agregar el state, arreglo de citas y pasar el citasIniciales
+  const [citas, guardarCitas]= useState(citasIniciales);
+
+//use effect
+useEffect(()=> {
+if (citasIniciales){
+  localStorage.setItem('citas', JSON.stringify(citas))
+} else{
+localStorage.setItem('citas', JSON.stringify([]))
+}
+},[citas])
+
+  const crearCita = cita => {
+    guardarCitas([
+      ...citas,
+      cita
+    ])
+  }
+//eliminar cita
+ const eliminarCita = id => {
+  const nuevasCitas = citas.filter(cita => cita.id !== id)
+  guardarCitas(nuevasCitas)
+ }
+ //cuando no hay citas
+ const titulo = citas.length === 0?'No hay Citas':'Administra tus Citas'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h2>Administrador de Pacientes</h2>
+       <div className='container'>
+        <div className='row'>
+          <div className='one-half column'>
+            <Formulario 
+            crearCita={crearCita}
+            
+            />
+          </div>
+          <div className='one-half column'>
+              <h2>{titulo}</h2>
+              {citas.map(cita=>(
+                <Cita 
+                key= {cita.id}
+                cita={cita}  
+                eliminarCita={eliminarCita}
+                />
+              ))
+              }
+          </div>          
+        </div>
+       </div>
+    </Fragment>
   );
 }
 
